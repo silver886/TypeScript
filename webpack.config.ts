eb6254fe59be1e518ebf7cd29fcfdb2969617205
Leo Liu <karta1041337@gmail.com>
2021-03-09 01:15:32 +0800

    Update web pack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,30 +1,31 @@
 import * as path from 'path';
-
-import * as webpack from 'webpack';
+import type * as webpack from 'webpack';
 
 const CONFIG: webpack.Configuration = {
     mode:   'production',
-    entry:  './src/index.ts',
+    entry:  path.resolve('./src/index.ts'),
+    output: {
+        path:     path.resolve('./dist'),
+        filename: 'bundle.js',
+    },
     module: {
-        rules: [
-            {
-                use:     'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
+        rules: [{
+            use:     'ts-loader',
+            exclude: /node_modules/u,
+        }],
     },
     resolve: {
-        extensions:     ['.ts', '.js'],
+        modules: [
+            path.resolve('node_modules'),
+            path.resolve('../../../../lambda/jenkins'),
+        ],
         preferRelative: true,
+        extensions:     ['.ts', '.js'],
         fallback:       {
             stream:  false,
             https:   false,
             process: false,
         },
-    },
-    output: {
-        filename: 'bundle.js',
-        path:     path.resolve(__dirname, 'dist'),
     },
 };
 
