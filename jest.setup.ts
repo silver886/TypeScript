b902a3fceea270a77963b8c60467af495d89dc30
Leo Liu <karta1041337@gmail.com>
2022-08-04 22:58:17 +0800

    Update jest config

---
+++ jest.setup.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/unambiguous
+beforeAll(() => {
+    console.log('beforeAll');
+});
+
+afterEach(() => {
+    console.log('afterEach');
+});
+
+afterAll(() => {
+    console.log('afterAll');
+});
